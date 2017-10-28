2c5b7679ae498ce5c50ceb5f8b7d70c3943eab44
huangguangda <2397923107@qq.com>
2017-10-28 17:21:00 +0800

    6

---
+++ app/src/main/java/cn/edu/gdmec/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,15 @@
+package cn.edu.gdmec.android.criminalintent;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Jack on 2017/10/28.
+ */
+
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment(){
+        return new CrimeListFragment ();
+    }
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,10 @@
+package cn.edu.gdmec.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Jack on 2017/10/28.
+ */
+
+public class CrimeListFragment extends Fragment {
+}
