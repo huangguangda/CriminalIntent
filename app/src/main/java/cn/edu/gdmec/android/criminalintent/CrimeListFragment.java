c944b76e2914c301ec4abc255368141d18f15941
huangguangda <2397923107@qq.com>
2017-11-05 11:27:06 +0800

    使用fragment argument

--- app/src/main/java/cn/edu/gdmec/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/cn/edu/gdmec/android/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package cn.edu.gdmec.android.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -31,12 +32,23 @@ public class CrimeListFragment extends Fragment {
         return view;
     }
 
+    @Override
+    public void onResume(){
+        super.onResume ();
+        updateUI ();
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get ( getActivity () );
         List<Crime> crimes = crimeLab.getCrimes ();
 
-        mAdapter = new CrimeAdapter ( crimes );
-        mCrimeRecyclerView.setAdapter ( mAdapter );
+        if (mAdapter == null){
+            mAdapter = new CrimeAdapter ( crimes );
+            mCrimeRecyclerView.setAdapter ( mAdapter );
+        }else {
+            mAdapter.notifyDataSetChanged ();
+        }
+
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
@@ -58,14 +70,17 @@ public class CrimeListFragment extends Fragment {
         public void bindCrime(Crime crime){
             mCrime = crime;
             mTitleTextView.setText ( mCrime.getTitle () );
-            mDataTextView.setText ( mCrime.getTitle ().toString () );
+            mDataTextView.setText ( mCrime.getDate ().toString () );
             mSolvedCheckBox.setChecked ( mCrime.isSolved () );
         }
         @Override
         public void onClick(View v){
-            Toast.makeText ( getActivity (),
-                    mCrime.getTitle () + " clicked!", Toast.LENGTH_SHORT)
-                    .show ();
+//            Toast.makeText ( getActivity (),
+//                    mCrime.getTitle () + " clicked!", Toast.LENGTH_SHORT)
+//                    .show ();
+            //Intent intent = new Intent ( getActivity (), CrimeActivity.class );
+            Intent intent = CrimeActivity.newIntent ( getActivity (), mCrime.getId () );
+            startActivity ( intent );
         }
     }
 
