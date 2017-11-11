9640a5dbcdc32878b9e9c73af8cf4ff0737d82de
huangguangda <2397923107@qq.com>
2017-11-11 13:13:07 +0800

    显示在工具栏上的菜单项图标完成

--- app/src/main/java/cn/edu/gdmec/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/cn/edu/gdmec/android/criminalintent/CrimeListFragment.java
@@ -23,6 +23,12 @@ public class CrimeListFragment extends Fragment {
     private CrimeAdapter mAdapter;
 
     @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate ( savedInstanceState );
+        setHasOptionsMenu ( true );
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState){
         View view = inflater.inflate ( R.layout.fragment_crime_list, container, false );
