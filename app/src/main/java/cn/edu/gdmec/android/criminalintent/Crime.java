2d9b65a7aa817dcd9e59f2fdfe12a466b86734d9
huangguangda <2397923107@qq.com>
2017-11-13 12:47:49 +0800

    252.1

--- app/src/main/java/cn/edu/gdmec/android/criminalintent/Crime.java
+++ app/src/main/java/cn/edu/gdmec/android/criminalintent/Crime.java
@@ -12,6 +12,7 @@ public class Crime {
     private String mTitle;
     private Date mDate;
     private boolean mSolved;
+    private String mSuspect;
 
     public Crime(){
         this(UUID.randomUUID ());
