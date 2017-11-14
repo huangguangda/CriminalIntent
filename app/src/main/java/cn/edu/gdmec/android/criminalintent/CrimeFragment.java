c8f03c2b7968ce3fc91383c29c32191ef991b4e0
huangguangda <2397923107@qq.com>
2017-11-14 13:20:26 +0800

    从联系人列表中获取联系人数据

--- app/src/main/java/cn/edu/gdmec/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/cn/edu/gdmec/android/criminalintent/CrimeFragment.java
@@ -3,6 +3,7 @@ package cn.edu.gdmec.android.criminalintent;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.text.Editable;
@@ -28,11 +29,17 @@ public class CrimeFragment extends Fragment {
     private static final String DIALOG_DATE = "DialogDate";
 
     private static final int REQUEST_DATE = 0;
+    private static final int REQUEST_CONTACT = 1;
 
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDataButton;
     private CheckBox mSolvedCheckBox;
+
+    private Button mSuspectButton;
+
+    private Button mReportButton;
+
     public static CrimeFragment newInstance(UUID crimeId){
         Bundle args = new Bundle (  );
         args.putSerializable ( ARG_CRIME_ID, crimeId );
@@ -103,6 +110,33 @@ public class CrimeFragment extends Fragment {
                 mCrime.setSolved ( isChecked );
             }
         } );
+        mReportButton = (Button) v.findViewById ( R.id.crime_report );
+        mReportButton.setOnClickListener ( new View.OnClickListener (){
+            public void onClick(View v){
+                Intent i = new Intent ( Intent.ACTION_SEND );
+                i.setType ( "text/plain" );
+                i.putExtra ( Intent.EXTRA_TEXT, getCrimeReport () );
+                i.putExtra ( Intent.EXTRA_SUBJECT,
+                        getString ( R.string.crime_report_subject ));
+                //选择器
+                i = Intent.createChooser ( i, getString ( R.string.send_report ) );
+                startActivity ( i );
+            }
+        } );
+
+        //260
+        final Intent pickContact = new Intent ( Intent.ACTION_PICK,
+                ContactsContract.Contacts.CONTENT_URI);
+        mSuspectButton = (Button) v.findViewById ( R.id.crime_suspect );
+        mSuspectButton.setOnClickListener ( new View.OnClickListener (){
+            public void onClick(View v){
+                startActivityForResult ( pickContact, REQUEST_CONTACT );
+            }
+        } );
+        if (mCrime.getSuspect () != null){
+            mSuspectButton.setText ( mCrime.getSuspect () );
+        }
+
         return v;
     }
 
